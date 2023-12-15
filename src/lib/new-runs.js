726c38fc4121a58c2cd52d7dd76fb903c0127b18
YorickH <yorick@teamleader.eu>
2023-12-15 15:50:38 +0100

    Fix url ansi encoding

--- src/lib/new-runs.js
+++ src/lib/new-runs.js
@@ -29,7 +29,7 @@ export default async function ({ octokit, workflow_id, run_id, after }) {
   // no workflow is running
   if (cancelling_for.length > 0) {
     // Function to generate a clickable link
-    const addClickableLink = (link, text) => `\u001B]8;;${link}\u0007${text}\u001B]8;;\u0007`;
+    const addClickableLink = (link, text) => `\u001B${link}\u0007${text}\u001B\u0007`;
 
     // Create clickable links for each item in the list
     const urlsString = cancelling_for.map(workflow => addClickableLink(workflow.html_url, workflow.id)).join(', ');
