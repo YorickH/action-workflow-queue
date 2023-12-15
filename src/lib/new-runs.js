80acabc738be12f7ef65bef6cc3cd9d808d16157
YorickH <yorick@teamleader.eu>
2023-12-15 14:44:09 +0100

    Add run urls to the cancel log

--- src/lib/new-runs.js
+++ src/lib/new-runs.js
@@ -15,7 +15,7 @@ export default async function ({ octokit, workflow_id, run_id, after }) {
   })
 
   // find any instances of the same workflow
-  const waiting_for = workflow_runs
+  const cancelling_for = workflow_runs
     // limit to currently running ones
     .filter(run => ['in_progress', 'queued', 'waiting', 'pending', 'action_required', 'requested'].includes(run.status))
     // exclude this one
@@ -23,18 +23,21 @@ export default async function ({ octokit, workflow_id, run_id, after }) {
     // get newer runs
     .filter(run => new Date(run.run_started_at) > after)
 
-  core.info(`ℹ️ found ${waiting_for.length} more recent workflow runs`)
-  core.debug(inspect(waiting_for.map(run => ({ id: run.id, name: run.name, run_started_at: run.run_started_at }))))
+  core.info(`ℹ️ found ${cancelling_for.length} more recent workflow runs`)
+  core.debug(inspect(cancelling_for.map(run => ({ id: run.id, name: run.name, run_started_at: run.run_started_at }))))
 
   // no workflow is running
-  if (waiting_for.length > 0) {
-    core.notice(`🛑 Cancelling this run because there is a more recent one running`)
+  if (cancelling_for.length > 0) {
+    // Extract the 'name' field from each run in cancelling_for array
+    const cancellingForUrls = cancelling_for.map(run => run.url);
+
+    // Join the names into a comma-separated string
+    const urlsString = cancellingForUrls.join(', ');
+    core.notice(`🛑 Cancelling this run because there is a more recent one running. More recent runs: ${namesString}`)
     octokit.request('POST /repos/{owner}/{repo}/actions/runs/{run_id}/cancel', {
       ...github.context.repo,
       run_id
     })
   }
-
-
 
 }
